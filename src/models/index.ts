f10fa26ecc949ab5e0b22f07a705753938c00655
dz1ki <dz1kill06@gmail.com>
2023-04-20 22:56:39 +0300

    save

---
+++ src/models/index.ts
@@ -0,0 +1,15 @@
+import * as config from "config";
+import { Sequelize } from "sequelize-typescript";
+import { User } from "./user";
+
+const connection = new Sequelize({
+  dialect: config.get("DBconfig.dialect"),
+  host: config.get("DBconfig.host"),
+  username: config.get("DBconfig.username"),
+  password: config.get("DBconfig.password"),
+  database: config.get("DBconfig.database"),
+  logging: true,
+  models: [User],
+});
+
+export default connection;
